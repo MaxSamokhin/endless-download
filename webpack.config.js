2462908228ddd3cbfb2e894a85ecfcbfce60078a
Max <samokhinmax@gmail.com>
2019-11-28 20:52:41 +0300

    now it`s work

--- webpack.config.js
+++ webpack.config.js
@@ -34,5 +34,10 @@ module.exports = {
         port: 7001,
         open: true,
         contentBase: './dist/',
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+            'Access-Control-Allow-Headers': 'content-type'
+        }
     }
 };
